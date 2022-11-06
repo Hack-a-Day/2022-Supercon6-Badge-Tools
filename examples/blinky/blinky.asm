b754e569e683c47e194bf885518ae0cd197ce0a5
Bradon Kanyid <rattboi@gmail.com>
2022-11-06 10:23:15 -0800

    Basic controls set up
    Other fixes
    Commit makefiles + nss pinout doc

--- examples/blinky/blinky.asm
+++ examples/blinky/blinky.asm
@@ -43,6 +43,9 @@ KeyReg      EQU 0xfd
 Dimmer      EQU 0xfe
 Random      EQU 0xff
 
+; user equs
+PlatformRows EQU 3
+
 ; init
 init: 
 mov r5, 4 ; page
@@ -54,7 +57,10 @@ mov [Page], r0
 mov r0, F_1_kHz; slow down a bit
 mov [Clock], r0
 
-mov r5, 5
+mov r6, 3
+mov r7, 10
+mov r8, PlatformRows
+mov r5, r8
 main:
 gosub check_keys
 gosub generate_row
@@ -62,19 +68,46 @@ mov r0, r1 ; set_bottom_row uses r0/r1 as the nibbles to display
 mov r1, r2
 gosub set_bottom_row
 gosub shift_screen_up
+;gosub draw_char
+inc r6
 mov r0, r5
 cp r0, 0
-skip nz, 1
-mov R5, 5
-goto main
+skip z, 1
+jr main
+mov r5, r8
+mov r6, 3
+jr main
 
 check_keys:
-mov R0, [0xFC] ; get keypress status
+mov R0, [KeyStatus] ; get keypress status
 bit R0,0; ; tests if not pressed, in Z
 skip z,3
-mov R0, [0xF3]
+mov R0, [WrFlags]
 btg R0,3
-mov [0xF3], R0
+mov [WrFlags], R0
+check_in:
+bit r3, 0  ; this is actually "in"
+skip nz, 1
+jr ck_left
+bit r3, 1
+skip nz, 1
+jr ck_right
+bit r3, 2
+skip nz, 1
+jr ck_b
+ret r0, 0
+
+ck_b:
+mov r0, PlatformRows
+mov r8, r0
+ret r0, 0
+
+ck_right:
+inc r8
+ret r0, 0
+
+ck_left:
+dec r8
 ret r0, 0
 
 set_bottom_row:
@@ -109,8 +142,6 @@ RET R0, 0
 
 generate_row:
 DEC R5
-MOV R0,R5
-CP R0, 0    
 SKIP Z, 1  
 JR gr_nz
 and r0, 0 ; clear carry
@@ -118,11 +149,15 @@ mov r0,[Random]
 rrc r0  ; logical shift right
 mov r1, 1
 mov r2, 0
-shift: ; 8-bit left shift by r0 bits
+cp r0, 0
+skip nz, 1
+jr gr_rnd_z
+gr_shift: ; 8-bit left shift by r0 bits
 add R1, R1
 adc R2, R2
 dsz r0
-jr shift
+jr gr_shift
+gr_rnd_z:
 MOV R0, R1 ; complement r1
 XOR R0, 0xF
 MOV R1, R0
@@ -133,4 +168,26 @@ RET R0, 0
 gr_nz:
 MOV R1, 0   
 MOV R2, 0   
+RET R0, 0
+
+draw_char:
+EXR 5
+MOV R5, R6
+MOV R1, 4
+MOV R2, 5
+MOV R4, 0b1000
+MOV R3, 0b0000
+dc_shiftloop:
+AND R0, 0
+RRC R4
+RRC R3
+DSZ R5
+JR dc_shiftloop
+MOV R0,[R2:R7]
+OR R0,R4
+MOV [R2:R7],R0
+MOV R0,[R1:R7]
+OR R0,R3
+MOV [R1:R7],R0
+EXR 5
 RET R0, 0
