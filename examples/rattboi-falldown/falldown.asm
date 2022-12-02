449cf7a8f8f13908f186a0399f86cc7ae066b3ce
Bradon Kanyid <rattboi@gmail.com>
2022-12-01 21:10:27 -0800

    Updated falldown game
    * Fixed bug in hiscore checking
    * Fixed bug with first erase character before draw character
    * Synchronize with user sync
    * Increase game speed over play time for more challenge
    * Rewrite to use new assembler features
      * local labels
      * synthetic instructions
      * SKIP labels
      * RS register alias for bit commands
    * Use more EQUs to remove hard-coded values
    * More comments
    
    Also committing the hexfile, to make it easier for people to play

--- examples/rattboi-falldown/falldown.asm
+++ examples/rattboi-falldown/falldown.asm
@@ -72,38 +72,45 @@ KeyRight EQU	13	; Data In
 
 ; user equs
 PlatformRows EQU 4
+ScrollSpeed EQU 6
 ScorePage  EQU 2
 ScoreLo    EQU 0
 ScoreUp    EQU 1
 HiScoreLo  EQU 2
 HiScoreUp  EQU 3
+Speed      EQU 4
 
 ; init
 init: 
 	; initialize high score
 	MOV R0, 0
-	MOV [ScorePage:2],R0
-	MOV [ScorePage:3],R0
+	MOV [ScorePage:HiScoreLo],R0
+	MOV [ScorePage:HiScoreUp],R0
+
 
 	; disable other LEDs
 	MOV R0, [WrFlags]
-	BTG R0,3
+	BTG R0, LedsOff
 	MOV [WrFlags], R0
 
-	MOV R0, F_3_kHz; slow down a bit
+	MOV R0, F_250_kHz ; run at fastest speed (using usersync)
 	MOV [Clock], R0
 
 game_start:
 	;initialize score
 	MOV R0, 0
-	MOV [ScorePage:0],R0
-	MOV [ScorePage:1],R0
+	MOV [ScorePage:ScoreLo],R0
+	MOV [ScorePage:ScoreUp],R0
 
-	MOV R5, 4 ; page
-	MOV R2, 0 ; storage for the ball's bit
+	; set initial speed
+	MOV R0, 14 
+	MOV [ScorePage:Speed], R0
 
-	MOV R0,R5  ; go to display page
+	MOV R0, 4 ; go to display page
 	MOV [Page], R0
+
+	MOV R0, 5
+	MOV	[Sync],R0
 
 	MOV R6, 3
 	MOV R7, 13
@@ -113,12 +120,12 @@ game_start:
 
 main:
 	GOSUB check_keys
-	GOSUB erase_char
 	MOV R9, R6 ; update last x-pos
 	GOSUB update_char
-	CP R0, 1
-	SKIP NZ, 2 ; skip 2 because `goto` is actually two instructions
+	CP R0, 1 ; game over?
+	SKIP NE, .keep_going ;no game over
 	GOTO game_start
+.keep_going:
 	GOSUB draw_char
 
 	; generate next row
@@ -127,77 +134,90 @@ main:
 	MOV R1, R2
 	GOSUB set_bottom_row
 
+	; wait for (speed) user syncs
+	MOV R0, [ScorePage:Speed]
+	MOV R1, R0
+	GOSUB user_sync
+
 	GOSUB shift_screen_up
-	GOSUB update_score
+	GOSUB erase_char
 
 	MOV R0, R5 ; r5 = how often to generate new row
 	CP R0, 0
-	SKIP Z, 1
+	SKIP Z, .reset_row_count
 	JR main
+.reset_row_count:
+	GOSUB update_score
 	MOV R5, R8
 	JR main
 
 get_badge_key:
 	MOV R0,[KeyStatus]
 	AND R0,0x6
-	SKIP NZ,1
+	SKIP NE,.key_pressed
 	RET R0,0xF
+.key_pressed:
 	MOV R0,[KeyReg]
 	CP R0,KeyLeft
-	SKIP NZ,1
+	SKIP NE,.not_left
 	RET R0,1 ; left
+.not_left:
 	CP R0,KeyB
-	SKIP NZ,1
+	SKIP NE,.not_b
 	RET R0,2 ; b
+.not_b:
 	CP R0,KeyRight
-	SKIP NZ,1
+	SKIP NE,.not_right
 	RET R0,3 ; right
+.not_right:
 	RET R0,0xf ; some other key
 
 check_keys:
 	GOSUB get_badge_key
-	CP R0, 0xf ; no key pressed
-	SKIP NZ, 1 ; some key is pressed, so skip
-	JR check_in  ; no badge key pressed, so now try IN port (Nintendo Super System)
+	CP  R0, 0xf ; no key pressed
+	SKIP NE, .which_badge_key ; some key is pressed, so skip
+	JR .check_in  ; no badge key pressed, so now try IN port (Nintendo Super System)
 ; check which badge key
-	CP R0, 1 ; left 
-	SKIP NZ, 1
-	JR ck_left
+.which_badge_key:
+	CP  R0, 1 ; left 
+	SKIP NE, .not_badge_left
+	JR .ck_left
+.not_badge_left:
 	CP R0, 3 ; right
-	SKIP NZ, 1
-	JR ck_right
+	SKIP NE, .not_badge_right
+	JR .ck_right
+.not_badge_right:
 	CP R0, 2 ; b
-	SKIP NZ, 1
-	JR ck_b
-
-check_in:
-	BIT R3, 0  ; r3 is actually "in" reg
-	SKIP NZ, 1
-	JR ck_left
-	BIT R3, 1
-	SKIP NZ, 1
-	JR ck_right
-	BIT R3, 2
-	SKIP NZ, 1
-	JR ck_b
+	SKIP NE, .check_in
+	JR .ck_b
+.check_in:
+	BIT RS, 0  ; r3 is actually "in" reg
+	SKIP NE, .not_left
+	JR .ck_left
+.not_left:
+	BIT RS, 1
+	SKIP NE, .not_right
+	JR .ck_right
+.not_right:
+	BIT RS, 2
+	SKIP NE, .not_b
+	JR .ck_b
+.not_b:
 	RET R0, 0
-
-ck_b:
+.ck_b:
 	MOV R0, PlatformRows
 	MOV R8, R0
 	RET R0, 0
-
-ck_right:
+.ck_right:
 	MOV R0, R6
-	CP R0, 7
-	SKIP Z, 1
+	CP  R0, 7
+	SKIP EQ, 1
 	INC R6
 	RET R0, 0
-
-ck_left:
+.ck_left:
 	MOV R0, R6
-	CP R0, 0
-	SKIP Z, 1
+	CP  R0, 0
+	SKIP EQ, 1
 	DEC R6
 	RET R0, 0
 
@@ -218,7 +238,7 @@ shift_screen_up:
 	MOV R1,4  ; original page 1
 	MOV R2,5  ; original page 2
 	MOV R8,15 ; number of rows to copy
-ssu_loop:
+.loop:
 	MOV R0,[R1:R6]
 	MOV [R1:R7],R0
 	MOV R0,[R2:R6]
@@ -226,38 +246,35 @@ ssu_loop:
 	INC R6
 	INC R7
 	DSZ R8
-	JR ssu_loop
+	JR .loop
 	EXR 0
 	RET R0, 0
 
 generate_row:
 	DEC R5
-	SKIP Z, 1
-	JR gr_nz
-	AND R0, 0 ; clear carry
-	MOV R0,[Random]
-	RRC R0  ; logical shift right
+	SKIP EQ, 3 ; .not_empty
+	; empty row, return zeros
+	MOV R1, 0
+	MOV R2, 0
+	RET R0, 0
+.not_empty:
+	MOV R0, [Random]
+	LSR R0
 	MOV R1, 1
 	MOV R2, 0
-	CP R0, 0
-	SKIP NZ, 1
-	JR gr_rnd_z
-gr_shift: ; 8-bit left shift by r0 bits
+	CP  R0, 0
+	SKIP NE, 1
+	JR .shift_done
+.shift: ; 8-bit left shift by r0 bits
 	ADD R1, R1
 	ADC R2, R2
 	DSZ R0
-	JR gr_shift
-gr_rnd_z:
-	MOV R0, R1 ; complement r1
-	XOR R0, 0xF
+	JR .shift
+.shift_done:
+	CPL R0, R1
 	MOV R1, R0
-	MOV R0, R2 ; complement r2
-	XOR R0, 0xF
+	CPL R0, R2
 	MOV R2, R0
-	RET R0, 0
-gr_nz:
-	MOV R1, 0
-	MOV R2, 0
 	RET R0, 0
 
 draw_char:
@@ -296,15 +313,15 @@ erase_char:
 	MOV R3, 0b0000
 	MOV R0, R5
 	CP R0, 0
-	SKIP NZ, 1
-	JR ec_xpos_z
-ec_shiftloop:
+	SKIP NZ, .shiftloop
+	JR .noshift
+.shiftloop:
 	AND R0, 0
 	RRC R4
 	RRC R3
 	DSZ R5
-	JR ec_shiftloop
-ec_xpos_z:
+	JR .shiftloop
+.noshift:
 	MOV R5,R7
 	DEC R5
 	MOV R0,[R2:R5]
@@ -318,113 +335,123 @@ ec_xpos_z:
 
 update_char:
 	EXR 6
-	MOV R5, R6
+	MOV R0, R6
 	MOV R1, 4
 	MOV R2, 5
 	MOV R4, 0b1000
 	MOV R3, 0b0000
-	MOV R0, R5
-	CP R0, 0
-	SKIP NZ, 1
-	JR uc_xpos_z
-uc_shiftloop:
-	AND R0, 0
+	CP  R0, 0
+	SKIP NE, 1
+	JR .noshift
+.shiftloop:
+	AND R0, 0xF
 	RRC R4
 	RRC R3
-	DSZ R5
-	JR uc_shiftloop
-uc_xpos_z:
-	MOV R5,R7
+	DSZ R0
+	JR .shiftloop
+.noshift:
+	MOV R5, R7
 	INC R5
 	; get left side, and invert
-	MOV R0,R4
-	CP R0, 0
-	SKIP NZ, 1
-	JR uc_testright
-	MOV R0,[R2:R5] ;0000
-	XOR R0, 0xF    ;1111
-	AND R0,R4      ;0100 = 0000 = z = collision
-	SKIP NZ, 1
-	JR collision
-
-uc_testright:
+	MOV R0, R4
+	CP  R0, 0
+	SKIP NE, 1
+	JR .testright
+	MOV R0, [R2:R5] ;0000
+	CPL R0          ;1111
+	AND R0, R4      ;0100 = 0000 = z = collision
+	SKIP NE, 1
+	JR .collision
+.testright:
 	; get right side and invert
-	MOV R0,R3
-	CP R0, 0
+	MOV R0, R3
+	CP  R0, 0
 	SKIP NZ, 1
-	JR no_collision
-	MOV R0,[R1:R5]
-	XOR R0, 0xF
-	AND R0,R3
+	JR .no_collision
+	MOV R0, [R1:R5]
+	CPL R0
+	AND R0, R3
 	SKIP NZ, 1
-	JR collision
-
-no_collision:
-	MOV R0,R7
-	CP R0, 13
-	SKIP Z, 1
+	JR .collision
+.no_collision:
+	MOV R0, R7
+	CP  R0, 13
+	SKIP EQ, 1
 	INC R7
 	EXR 6
 	RET R0, 0
-
-collision:
+.collision:
 	DEC R7
-	MOV R0, R7
-	CP R0, 15 ; wrapped, so dead
-	SKIP NZ, 1
-	JR dead
+	SKIP NC, .dead ; NC mean R7 is now 15, so we wrapped, and are thus dead
 	EXR 6
 	RET R0, 0
-
-dead:
-	MOV R8, 15
+.dead:
+	MOV R8, 0  ; # of rows to scroll/display (effectively 16, with DSZ)
 	GOSUB check_high_score
-	CP R0, 1
-	SKIP NZ, 1
-	JR hs_loop
-d_loop:
+	CP  R0, 1
+	SKIP NE, .loop
+	JR .checkerboard
+.loop:
 	MOV R0, 15 ; set to full bar
 	MOV R1, 15
 	GOSUB set_bottom_row
 	GOSUB shift_screen_up
+	MOV R0, ScrollSpeed
+	MOV R1, R0
+	GOSUB user_sync
 	DSZ R8
-	JR d_loop
-	JR d_check_button
-hs_loop:
+	JR .loop
+	JR .wait_for_button
+.checkerboard:
 	MOV R6, 10 ; set to checkboard pattern
-hs_inner:
+.checkerboard_loop:
+	CPL R0, R6
+	MOV R6, R0
+	GOSUB shift_screen_up
 	MOV R0, R6
 	MOV R1, R0
 	GOSUB set_bottom_row
-	GOSUB shift_screen_up
-	MOV R0, R6
-	XOR R0, 0xF
-	MOV R6, R0
+	MOV R0, ScrollSpeed
+	MOV R1, R0
+	GOSUB user_sync
 	DSZ R8
-	JR hs_inner
-d_check_button:
+	JR .checkerboard_loop
+	MOV R4, 15
+.wait_for_button:
+	MOV R0, ScrollSpeed
+	MOV R1, R0
+	GOSUB user_sync
+	MOV R0, R4
+	MOV [Dimmer], R0
+	DEC R0
+	MOV R4, R0
 	GOSUB get_badge_key
-	CP R0, 0xf ; no key pressed
-	SKIP NZ, 1 ; some key is pressed, so skip
-	JR d_check_in  ; no badge key pressed, so now try IN port (Nintendo Super System)
-; check which badge key
+	CP R0, 0xf    ; no key pressed
+	SKIP NE, 1    ; some key is pressed, so skip
+	JR .check_in  ; no badge key pressed, so now try IN port (Nintendo Super System)
+  ; check which badge key
 	CP R0, 2 ; b
 	SKIP NZ, 1
-	JR d_button_pushed
-d_check_in:
-	BIT R3, 2
-	SKIP Z, 1
-	JR d_check_button
-d_button_pushed:
+	JR .button_pushed
+.check_in:
+	BIT RS, 2 ; check for IN port bit 2 (b button)
+	SKIP EQ, .button_pushed
+	JR .wait_for_button
+.button_pushed:
+	MOV R0, 15
+	MOV [Dimmer], R0
 	; clear screen before restarting
-	MOV R8, 15
-d_clearscreen_loop:
+	MOV R8, 0
+.clearscreen_loop:
 	MOV R0, 00 ; set to empty
 	MOV R1, 00
 	GOSUB set_bottom_row
 	GOSUB shift_screen_up
+	MOV R0, ScrollSpeed
+	MOV R1, R0
+	GOSUB user_sync
 	DSZ R8
-	JR d_clearscreen_loop
+	JR .clearscreen_loop
 	EXR 6
 	RET R0,1
 
@@ -441,6 +468,12 @@ update_score:
 	MOV [ScorePage:ScoreUp], R0
 	MOV R0, R1
 	MOV [ScorePage:ScoreLo], R0
+	CP R0, 0
+	SKIP NZ, .return
+	MOV R0, [ScorePage:Speed]
+	DEC R0
+	MOV [ScorePage:Speed], R0
+.return:
 	EXR 0
 	RET R0, 0
 
@@ -455,19 +488,37 @@ check_high_score:
 	MOV R0, [ScorePage:HiScoreUp]
 	MOV R1, R0
 
-	SUB R2,R4
-	SKIP C, 3
-	SBB R1,R3
-	SKIP C, 1
-	JR chs_set
+	; R1:R2 = hiscore
+	; R3:R4 = score
+
+	SUB R3, R1 ; MSB(score - hiscore)
+	SKIP  Z, .maybe_highscore
+	SKIP NC, .no_highscore ; MSB(hiscore >= score)
+	JR .set_highscore
+.maybe_highscore:
+	SUB R4, R2 ; LSB(hiscore - score)
+	SKIP Z, .no_highscore
+	SKIP C, .set_highscore
+.no_highscore:
 	EXR 0
 	RET r0, 0
-chs_set:
-	MOV R0, [ScorePage:ScoreLo] ; load curr score into r4:r3
+.set_highscore:
+	MOV R0, [ScorePage:ScoreLo] ; load curr score into r1:r0
 	MOV R1, R0
 	MOV R0, [ScorePage:ScoreUp]
-	MOV [ScorePage:HiScoreUp], R0
+	MOV [ScorePage:HiScoreUp], R0 ; store r1:r0 -> hiscore
 	MOV R0, R1
 	MOV [ScorePage:HiScoreLo], R0
 	EXR 0
 	RET R0, 1
+
+user_sync:
+	MOV	R0,[RdFlags]
+	BIT	R0, 0	; Bit 0 is UserSync.
+	SKIP NE,.sync
+	JR	user_sync
+.sync:
+	DSZ r1
+	JR  user_sync
+	RET	R0, 0
+
